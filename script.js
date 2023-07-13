721bba51aed8d1e075eae39979e02c9d89f65233
Yatendra Kumar <134841174+yatenk9935@users.noreply.github.com>
2023-07-13 17:21:15 +0530

    Landing page

---
+++ script.js
@@ -0,0 +1,84 @@
+$("#Crsl-2").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplaytimeout: 3000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$("#Crsl-3").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplaytimeout: 3000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$("#Crsl-4").owlCarousel({
+  loop: true,
+  margin: false,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplaytimeout: 3000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+
+$("#slider-2").owlCarousel({
+  loop: false,
+  autoplay: false,
+  nav: true,
+  dots: false,
+  navText: [
+    "<i class='bi bi-chevron-left'></i>",
+    "<i class='bi bi-chevron-right'></i>",
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
